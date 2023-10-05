86a94250177c09fa6b1336070a36a3dbce97852c
BBBomber <yashasvi.chopra555@gmail.com>
2023-10-06 00:18:36 +0530

    nm

--- CozyFall/Assets/Characters/Bosses/Grumpkin/Grumpkin.cs
+++ CozyFall/Assets/Characters/Bosses/Grumpkin/Grumpkin.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class Grumpkin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float attackRadius;
+    [SerializeField]
+    private int health;
+    [SerializeField]
+    private int speed;
+
+    public Transform playerTransform;
+
+
+    
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer sr;
+
+    private enum GrumpkinState
     {
+        follow,
+        attack,
+        jump,
+        shoot
+    }
+
+    private GrumpkinState bossState = GrumpkinState.follow;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //animator = GetComponent<Animator>();
+        //sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        switch (bossState)
+        {
+            case GrumpkinState.follow:
+
+                Follow();
+                break;
+
+            case GrumpkinState.attack:
+
+                //animator.SetBool("IsMoving", true);
+                break;
+
+            case GrumpkinState.jump:
+
+                //animator.SetTrigger("Dash");
+                break;
+
+            case GrumpkinState.shoot:
+
+                // animator.SetTrigger("Attack");
+                break;
+        }
+
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Follow()
     {
-        
+        this.transform.Translate((playerTransform.position - this.transform.position).normalized * speed * Time.deltaTime);
     }
+
 }
